8ce3f87f5cf0547df4a05f85c6d39000703cdb57
Brendon Bown <bhbochikens@gmail.com>
2017-12-22 20:18:26 -0600

    Completed welcome frame w/o format or function

--- src/view/WelcomeFrame.java
+++ src/view/WelcomeFrame.java
@@ -1,8 +1,6 @@
 package view;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class WelcomeFrame extends JFrame {
@@ -14,6 +12,7 @@ public class WelcomeFrame extends JFrame {
 		//Format JFrame
 		super("ON A ROLL");
 		setLookAndFeel();
+		setSize(new Dimension(600, 150));
 		
 		//Make a JPanel to add everything to and format it
 		JPanel panel = new JPanel();
@@ -21,18 +20,28 @@ public class WelcomeFrame extends JFrame {
 		//Make the welcome greeting
 		JLabel welcomeGreetingLine1 = new JLabel("Welcome to ON A ROLL!", JLabel.CENTER);
 		JLabel welcomeGreetingLine2 = new JLabel("To get started, load a roster or create a new one", JLabel.CENTER);
+		welcomeGreetingLine1.setFont(new Font(Font.SERIF, Font.BOLD, 20));
+		welcomeGreetingLine2.setFont(new Font(Font.SERIF, Font.PLAIN, 18));
 		panel.add(welcomeGreetingLine1);
 		panel.add(welcomeGreetingLine2);
 		
-		//Add a place to put the location and a button for browsing
+		//Add a place to find a file
 		// TODO make a specialized browsing button that inserts location in location
-		JTextField location = new JTextField(15);
+		JPanel location = new JPanel();
+		location.setLayout(new BoxLayout(location, BoxLayout.X_AXIS));
+		JTextField locationText = new JTextField(15);
 		JButton browse = new JButton("Browse");
+		location.add(locationText);
+		location.add(browse);
 		panel.add(location);
-		panel.add(browse);
 		
+		//Add a load button
 		JButton load = new JButton("Load");
 		panel.add(load);
+		
+		//Add a new button
+		JButton newRoster = new JButton("New");
+		panel.add(newRoster);
 		
 		//Add panel to the frame
 		add(panel);
@@ -41,7 +50,6 @@ public class WelcomeFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Pack and make visible
-		pack();
 		setVisible(true);
 		
 	}
